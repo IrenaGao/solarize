8e05618ab13439ead6a1d5b19584c87a048bf60f
Irena Gao <irenagao2013@gmail.com>
2020-03-26 16:07:59 -0400

    added files

---
+++ order.php
@@ -0,0 +1,427 @@
+<?php
+
+// $db = new PDO('sqlite:solarize.sqlite');
+
+// $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+include("includes/init.php");
+
+$db = open_sqlite_db("secure/solarize.sqlite");
+
+// const SIZE OPTIONS = [
+
+//   "name" => "By Name",
+
+//   "calorie" => "By Calorie",
+
+//   "place" => "By Place",
+
+//   "month_consumed" => "By Month Consumed",
+
+// ];
+
+if (isset($_POST["submit_form"])) {
+	var_dump("hello");
+	$valid = TRUE;
+
+	$size = filter_input(INPUT_POST, 'size', FILTER_SANITIZE_STRING);
+	$location = filter_input(INPUT_POST, 'location', FILTER_SANITIZE_STRING);
+	$hoursperday = filter_input(INPUT_POST, 'hoursperday', FILTER_SANITIZE_STRING);
+	$daysperyear = filter_input(INPUT_POST, 'daysperyear', FILTER_SANITIZE_STRING);
+}
+
+if ($valid) {
+	$sql = "INSERT INTO solarize (size, location, hoursperday, daysperyear) VALUES (:size, :location, :hoursperday, :daysperyear)";
+	$params = array(
+		':size' => $size,
+		':location' => $location,
+		':hoursperday' => $hoursperday,
+		':daysperyear' =>$daysperyear
+	);
+
+	$result = exec_sql_query($db, $sql, $params);
+}
+
+ ?>
+
+<!DOCTYPE HTML>
+
+<!--
+
+	Industrious by TEMPLATED
+
+	templated.co @templatedco
+
+	Released for free under the Creative Commons Attribution 3.0 license (templated.co/license)
+
+-->
+
+<html>
+
+	<head>
+
+		<title>Solarize - Purchase</title>
+
+		<meta charset="utf-8" />
+
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+
+		<meta name="description" content="" />
+
+		<meta name="keywords" content="" />
+
+		<link rel="stylesheet" href="assets/css/main.css" />
+
+	</head>
+
+	<body class="is-preload">
+
+
+
+		<!-- Header -->
+
+			<header id="header">
+
+				<a class="logo" href="index.php">Solarize</a>
+
+				<nav>
+
+					<a href="#menu">Menu</a>
+
+				</nav>
+
+			</header>
+
+
+
+		<!-- Nav -->
+
+			<nav id="menu">
+
+				<ul class="links">
+
+					<li><a href="index.php">Home</a></li>
+
+					<li><a href="elements.php">Elements</a></li>
+
+					<li><a href="order.php">Order</a></li>
+
+				</ul>
+
+			</nav>
+
+
+
+		<!-- Heading -->
+
+			<div id="heading" >
+
+				<h1>Custom Order</h1>
+
+			</div>
+
+
+
+		<!-- Main -->
+
+			<section id="main" class="wrapper">
+
+				<div class="inner">
+
+					<div class="content">
+
+						<div id="order_div">
+
+							<form id="order_form" method="post">
+
+								<header><h2>Custom Order</h2></header>
+
+								<ul>
+
+	        				<li class="order_field"><span class="order_field_title">Size: </span>
+
+										<select id="size_input" name="size" required>
+
+											<option value"" disabled selected>- Select a size -</option>
+
+											<option value="5'x8'">5'x8'</option>
+
+											<option value="6'x8'">6'x8'</option>
+
+											<option value="6'x12'">6'x12'</option>
+
+											<option value="6'x14'">6'x14'</option>
+
+											<option value="7'x14'">7'x14'</option>
+
+										</select>
+
+									</li></br></br>
+
+	        				<li class="order_field"><span class="order_field_title">Location: </span>
+
+										<select id="location_input" name="location" required>
+
+											<option value"" disabled selected>- Select a state -</option>
+
+											<option value="AL">Alabama</option>
+
+											<option value="AK">Alaska</option>
+
+											<option value="AZ">Arizona</option>
+
+											<option value="AR">Arkansas</option>
+
+											<option value="CA">California</option>
+
+											<option value="CO">Colorado</option>
+
+											<option value="CT">Connecticut</option>
+
+											<option value="DE">Delaware</option>
+
+											<option value="DC">District Of Columbia</option>
+
+											<option value="FL">Florida</option>
+
+											<option value="GA">Georgia</option>
+
+											<option value="HI">Hawaii</option>
+
+											<option value="ID">Idaho</option>
+
+											<option value="IL">Illinois</option>
+
+											<option value="IN">Indiana</option>
+
+											<option value="IA">Iowa</option>
+
+											<option value="KS">Kansas</option>
+
+											<option value="KY">Kentucky</option>
+
+											<option value="LA">Louisiana</option>
+
+											<option value="ME">Maine</option>
+
+											<option value="MD">Maryland</option>
+
+											<option value="MA">Massachusetts</option>
+
+											<option value="MI">Michigan</option>
+
+											<option value="MN">Minnesota</option>
+
+											<option value="MS">Mississippi</option>
+
+											<option value="MO">Missouri</option>
+
+											<option value="MT">Montana</option>
+
+											<option value="NE">Nebraska</option>
+
+											<option value="NV">Nevada</option>
+
+											<option value="NH">New Hampshire</option>
+
+											<option value="NJ">New Jersey</option>
+
+											<option value="NM">New Mexico</option>
+
+											<option value="NY">New York</option>
+
+											<option value="NC">North Carolina</option>
+
+											<option value="ND">North Dakota</option>
+
+											<option value="OH">Ohio</option>
+
+											<option value="OK">Oklahoma</option>
+
+											<option value="OR">Oregon</option>
+
+											<option value="PA">Pennsylvania</option>
+
+											<option value="RI">Rhode Island</option>
+
+											<option value="SC">South Carolina</option>
+
+											<option value="SD">South Dakota</option>
+
+											<option value="TN">Tennessee</option>
+
+											<option value="TX">Texas</option>
+
+											<option value="UT">Utah</option>
+
+											<option value="VT">Vermont</option>
+
+											<option value="VA">Virginia</option>
+
+											<option value="WA">Washington</option>
+
+											<option value="WV">West Virginia</option>
+
+											<option value="WI">Wisconsin</option>
+
+											<option value="WY">Wyoming</option>
+
+										</select>
+
+									</li><br/></br>
+
+									<li class="order_field"><span class="order_field_title">Text Box: </span>
+
+										<p id="order_info_text"> Lorem ipsum dolor sit accumsan
+
+											interdum nisi, quis tincidunt felis sagittis eget. tempus
+
+											euismod. Magna et cursus lorem faucibus vestibulum. Blandit
+
+											adipiscing eu felis iaculis volutpat ac adipiscing accumsan
+
+											eu faucibus. Integer ac pellentesque praesent tincidunt
+
+											felis sagittis eget. tempus euismod tempus. Vestibulum ante
+
+											ipsum primis in faucibus vestibulum. Blandit adipiscing eu
+
+											felis iaculis volutpat ac adipiscing accumsan eu faucibus.
+
+											Integer ac pellentesque praesent tincidunt felis sagittis
+
+											eget. tempus euismod. Vestibulum ante ipsum primis in faucibus
+
+											vestibulum. Blandit adipiscing eu felis iaculis volutpat
+
+											ac adipiscing accumsan eu faucibus. Integer ac sed amet praesent.
+
+											Nunc lacinia ante nunc ac gravida lorem ipsum dolor sit amet
+
+											dolor feugiat consequat. </p>
+
+									</li>
+
+									<li class="order_field"><span class="order_field_title">Hours Per Day: </span>
+
+										<input id="hours_input" type="number" step="0.01" name="hoursperday" placeholder="Hours" min="0" max="24" required/>
+
+        					</li></br></br>
+
+									<li class="order_field"><span class="order_field_title">Days Per Year: </span>
+
+										<input id="days_input" type="number" name="daysperyear" placeholder="Days" min="0" max="365" required/>
+
+        					</li></br></br></br>
+
+
+
+					        <li>
+
+					          <button id="order_form_button" type="submit" name="submit_form">Submit</button>
+
+					        </li>
+
+					      </ul><hr/>
+
+								<h3>Text Box: </h3>
+
+								<p>In arcu accumsan arcu adipiscing accumsan orci ac. Felis id enim aliquet. Accumsan ac integer lobortis commodo ornare aliquet accumsan erat tempus amet porttitor. Ante commodo blandit adipiscing integer semper orci eget. Faucibus commodo adipiscing mi eu nullam accumsan morbi arcu ornare odio mi adipiscing nascetur lacus ac interdum morbi accumsan vis mi accumsan ac praesent.</p>
+
+								<p>Felis sagittis eget tempus primis in faucibus vestibulum. Blandit adipiscing eu felis iaculis volutpat ac adipiscing accumsan eu faucibus. Integer ac pellentesque praesent tincidunt felis sagittis eget. tempus euismod. Magna sed etiam ante ipsum primis in faucibus vestibulum. Blandit adipiscing eu ipsum primis in faucibus vestibulum. Blandit adipiscing eu felis iaculis volutpat ac adipiscing accumsan eu faucibus lorem ipsum dolor sit amet nullam.</p>
+
+							</form>
+
+						</div>
+
+					</div>
+
+				</div>
+
+			</section>
+
+
+
+		<!-- Footer -->
+
+			<footer id="footer">
+
+				<div class="inner">
+
+					<div class="content">
+
+						<section>
+
+							<h3>Accumsan montes viverra</h3>
+
+							<p>Nunc lacinia ante nunc ac lobortis. Interdum adipiscing gravida odio porttitor sem non mi integer non faucibus ornare mi ut ante amet placerat aliquet. Volutpat eu sed ante lacinia sapien lorem accumsan varius montes viverra nibh in adipiscing. Lorem ipsum dolor vestibulum ante ipsum primis in faucibus vestibulum. Blandit adipiscing eu felis iaculis volutpat ac adipiscing sed feugiat eu faucibus. Integer ac sed amet praesent. Nunc lacinia ante nunc ac gravida.</p>
+
+						</section>
+
+						<section>
+
+							<h4>Sem turpis amet semper</h4>
+
+							<ul class="alt">
+
+								<li><a href="#">Dolor pulvinar sed etiam.</a></li>
+
+								<li><a href="#">Etiam vel lorem sed amet.</a></li>
+
+								<li><a href="#">Felis enim feugiat viverra.</a></li>
+
+								<li><a href="#">Dolor pulvinar magna etiam.</a></li>
+
+							</ul>
+
+						</section>
+
+						<section>
+
+							<h4>Magna sed ipsum</h4>
+
+							<ul class="plain">
+
+								<li><a href="#"><i class="icon fa-twitter">&nbsp;</i>Twitter</a></li>
+
+								<li><a href="#"><i class="icon fa-facebook">&nbsp;</i>Facebook</a></li>
+
+								<li><a href="#"><i class="icon fa-instagram">&nbsp;</i>Instagram</a></li>
+
+								<li><a href="#"><i class="icon fa-github">&nbsp;</i>Github</a></li>
+
+							</ul>
+
+						</section>
+
+					</div>
+
+					<div class="copyright">
+
+						&copy; Untitled. Photos <a href="https://unsplash.co">Unsplash</a>, Video <a href="https://coverr.co">Coverr</a>.
+
+					</div>
+
+				</div>
+
+			</footer>
+
+
+
+		<!-- Scripts -->
+
+			<script src="assets/js/jquery.min.js"></script>
+
+			<script src="assets/js/browser.min.js"></script>
+
+			<script src="assets/js/breakpoints.min.js"></script>
+
+			<script src="assets/js/util.js"></script>
+
+			<script src="assets/js/main.js"></script>
+
+
+
+	</body>
+
+</html>
